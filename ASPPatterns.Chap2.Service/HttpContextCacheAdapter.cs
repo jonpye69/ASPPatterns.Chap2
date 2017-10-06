d667ff942bb7f298f8b78497e0e5dd2da23ee8aa
jonpye <jon.pye@nbt.nhs.uk>
2017-10-06 10:43:37 +0100

    cache storage for product (interface for cache storage, adapter pattern used for cache context), also with null object pattern used.

---
+++ ASPPatterns.Chap2.Service/HttpContextCacheAdapter.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+
+namespace ASPPatterns.Chap2.Service
+{
+    /// <summary>
+    /// Implements the adapter pattern - used to let classes with incompatible interfaces work with one another
+    /// </summary>
+    public class HttpContextCacheAdapter : ICacheStorage
+    {
+        public void Remove(string key)
+        {
+            HttpContext.Current.Cache.Remove(key);
+        }
+        public void Store(string key, object data)
+        {
+            HttpContext.Current.Cache.Insert(key, data);
+        }
+        public T Retrieve<T>(string key)
+        {
+            T itemStored = (T)HttpContext.Current.Cache.Get(key);
+            if (itemStored == null)
+                itemStored = default(T);
+            return itemStored;
+        }
+    }
+}
---
+++ ASPPatterns.Chap2.Service/ProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ASPPatterns.Chap2.Service
+{
+    public class ProductRepository : IProductRepository
+    {
+        public IList<Product> GetAllProductsIn(int categoryId)
+        {
+            IList<Product> products = new List<Product>();
+            // Database operation to populate products …
+            return products;
+        }
+    }
+}
